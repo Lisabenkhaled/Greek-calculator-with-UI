07e5f4fc4b4316edb99aaaffdf2e9843fd8403c9
lpvu-lou <phuongglou@gmail.com>
2025-11-27 06:26:03 +0100

    error handling v2

--- GreekCalculatorWeb.Client/obj/Debug/net9.0/GreekCalculatorWeb.Client.AssemblyInfo.cs
+++ GreekCalculatorWeb.Client/obj/Debug/net9.0/GreekCalculatorWeb.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreekCalculatorWeb.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d76072df479928c7037125176145d4dfa82331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b590a2744a80e9b4f8e1c3308029be5e3fc23bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreekCalculatorWeb.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreekCalculatorWeb.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GreekCalculatorWeb/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
+++ GreekCalculatorWeb/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d76072df479928c7037125176145d4dfa82331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b590a2744a80e9b4f8e1c3308029be5e3fc23bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
